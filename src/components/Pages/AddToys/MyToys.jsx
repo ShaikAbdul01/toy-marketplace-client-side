c693b5ea00c9b9000e8e73505ad26c3ba5321306
Shaik Abdul Ahad <118795198+ShaikAbdul01@users.noreply.github.com>
2023-05-20 20:32:11 +0600

    price sort added

--- src/components/Pages/AddToys/MyToys.jsx
+++ src/components/Pages/AddToys/MyToys.jsx
@@ -1,23 +1,24 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../AuthProvider/AuthProvider";
-import { Link } from "react-router-dom";
 import Mytoy from "./Mytoy";
 import Swal from "sweetalert2";
+import { FaArrowCircleDown, FaArrowCircleUp } from "react-icons/fa";
 
 const MyToys = () => {
   const { user } = useContext(AuthContext);
   const [removes, setRemoves] = useState([]);
   const userEmail = user?.email;
+  const [myToys, setMyToys] = useState([]);
   //   console.log(userEmail);
 
-  const [myToys, setMyToys] = useState([]);
+  const [sortOrder, setSortOrder] = useState("asc"); // Sort order state
 
   useEffect(() => {
     fetch(`http://localhost:5000/addToys/email/${userEmail}`)
       .then((res) => res.json())
       .then((data) => setMyToys(data));
   }, []);
-//   console.log(myToys);
+  //   console.log(myToys);
 
   const handleDelete = (_id) => {
     console.log(_id);
@@ -50,27 +51,44 @@ const MyToys = () => {
       }
     });
   };
+
+  const handleSort = () => {
+    const sortedToys = [...myToys].sort((a, b) => {
+      if (sortOrder === "asc") {
+        return a.price - b.price;
+      } else {
+        return b.price - a.price;
+      }
+    });
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    setMyToys(sortedToys);
+  };
   return (
     <>
-      {/*  */}
       <div className="overflow-x-auto w-full">
         <table className="table w-full">
-          {/* head */}
           <thead>
             <tr>
               <th>Toy picture</th>
               <th>Car Name</th>
               <th>Seller Name</th>
               <th>Sub Category</th>
-              <th>Price</th>
+              <th>
+                Price{" "}
+                <button onClick={handleSort}>
+                  {sortOrder === "asc" ? (
+                    <FaArrowCircleUp></FaArrowCircleUp>
+                  ) : (
+                    <FaArrowCircleDown></FaArrowCircleDown>
+                  )}
+                </button>
+              </th>
               <th>Quantity</th>
               <th>Rating</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
-            {/* row 1 */}
-
             {myToys.map((toy) => (
               <Mytoy
                 key={toy._id}
